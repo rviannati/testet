11b992668defac7237828e4e33f8b7f7353fd0f9
rviannati <119507755+rviannati@users.noreply.github.com>
2023-07-21 15:50:05 -0300

    Update TesteApplicationTests.java

--- src/test/java/br/com/confitec/teste/TesteApplicationTests.java
+++ src/test/java/br/com/confitec/teste/TesteApplicationTests.java
@@ -1,13 +1,26 @@
 package br.com.confitec.teste;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TesteApplicationTests {
 
+	@Mock
+	private ApoliceSeguroService apoliceMock;
+
+	@BeforeEach
+	public void setUp() {
+		// Inicializar os mocks antes de cada teste
+		MockitoAnnotations.openMocks(this);
+	}
+
 	@Test
-	void contextLoads() {
+	public void testCalcularPlanoPagamentoComJuros() {
 	}
 
 }
